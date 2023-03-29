3691088c729116ae20b691bcf73f0aaf395ec105
Bernardo Porto e Silva Souza <bernardo.souza@cwi.com.br>
2023-03-29 16:07:29 -0300

    Initial Template

--- app/src/main/java/br/feevale/calculator/MainActivity.kt
+++ app/src/main/java/br/feevale/calculator/MainActivity.kt
@@ -2,10 +2,24 @@ package br.feevale.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    var value: String = "0"
+    val displayText1: TextView = findViewById(R.id.display_text)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClickValue(button: View){
+        val button: Button = button as Button
+        value += button.text
+        Log.d("TextButton", "${button.text.toString()} - ${displayText1.text} - ${value}")
     }
 }
